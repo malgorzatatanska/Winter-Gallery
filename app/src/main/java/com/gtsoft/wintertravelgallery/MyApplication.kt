1ce15922a5d214ff1de0a44e0ad4f0742eddd2aa
malgorzatatanska <malgosiatanska1@gmail.com>
2026-01-13 22:13:42 +0100

    Add image caching configuration

---
+++ app/src/main/java/com/gtsoft/wintertravelgallery/MyApplication.kt
@@ -0,0 +1,31 @@
+package com.gtsoft.wintertravelgallery
+
+import android.app.Application
+import android.content.Context
+import coil3.ImageLoader
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import coil3.request.CachePolicy
+
+class MyApplication : Application(), SingletonImageLoader.Factory {
+
+    override fun newImageLoader(context: Context): ImageLoader {
+        return ImageLoader.Builder(context)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context, 0.1)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.03)
+                    .build()
+            }
+            .build()
+    }
+}
